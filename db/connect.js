8765149d5e10c790234e319a583869e74252b675
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-11-15 09:00:04 +0600

    store api filter added

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-}
+const connectDB = async (url) => {
+  try {
+    await mongoose.connect(url);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
